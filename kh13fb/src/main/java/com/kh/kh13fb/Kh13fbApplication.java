e88bf4b2e0b4a7d0dfa96292f5d4b10ac0e416d6
HW5434 <l990628l@gmail.com>
2024-04-26 10:31:38 +0900

    파이널 프로젝트 폴더 생성

---
+++ kh13fb/src/main/java/com/kh/kh13fb/Kh13fbApplication.java
@@ -0,0 +1,13 @@
+package com.kh.kh13fb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kh13fbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kh13fbApplication.class, args);
+	}
+
+}
