54590fe50db256e677871e02b3deb0a1470792cc
HW5434 <l990628l@gmail.com>
2024-04-29 17:06:35 +0900

    QnA CRUD구현중

--- kh13fb/src/main/java/com/kh/kh13fb/dao/QnaDao.java
+++ kh13fb/src/main/java/com/kh/kh13fb/dao/QnaDao.java
@@ -18,4 +18,17 @@ public class QnaDao {
 		return sqlSession.selectList("qna.list");
 	}
 	
+	//시퀀스
+	public int sequence() {
+		return sqlSession.selectOne("qna.sequence");
+	}
+	//등록 메소드
+	public void insert(QnaDto qnaDto) {
+		sqlSession.insert("qna.save", qnaDto);
+	}
+
+	public QnaDto selectOne(int qnaNo) {
+		return sqlSession.selectOne("qna.find", qnaNo);
+	}
+	
 }
