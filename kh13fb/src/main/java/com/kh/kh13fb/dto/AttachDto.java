8eaa3b68d860b8ae9e45e0c170eaae942ee23c85
skdiatod <skdiatod@naver.com>
2024-05-01 01:17:24 +0900

    attach 구문 구성중

---
+++ kh13fb/src/main/java/com/kh/kh13fb/dto/AttachDto.java
@@ -0,0 +1,15 @@
+package com.kh.kh13fb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class AttachDto {
+
+	private int attachNo;
+	private String attachName;
+	private String attachType;
+	private long attachSize;
+}
