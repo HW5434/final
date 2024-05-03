bbf9022db3fbd16b4750f96e6622595f123b25ef
yellll1n <yr1230_1103@hanmail.net>
2024-05-03 19:11:08 +0900

    로그인 구현중

--- kh13fb/src/main/java/com/kh/kh13fb/restcontroller/MemberRestController.java
+++ kh13fb/src/main/java/com/kh/kh13fb/restcontroller/MemberRestController.java
@@ -83,65 +83,6 @@ public class MemberRestController {
 		return ResponseEntity.ok().build();
 	}
 	
-	@PostMapping("/login")
-	public ResponseEntity<MemberLoginVO> login(@RequestBody MemberDto memberDto){
-		//아이디로 정보조회
-		MemberDto findDto = memberDao.selectOne(memberDto.getMemberNo());
-		if(findDto == null) {
-			return ResponseEntity.notFound().build();//404
-		}
-		//비밀번호 비교(암호화가 있다면 코드가 달라질 수 있음)
-		boolean isValid = findDto.getMemberPw().equals(
-					memberDto.getMemberPw());
-		if(isValid) {//성공
-			String accessToken = jwtService.createAccessToken(findDto);
-			String refreshToken = jwtService.createRefreshToken(findDto);
-			return ResponseEntity.ok().body(MemberLoginVO.builder()
-						.memberNo(findDto.getMemberNo())//회원아이디
-						.memberGrade(findDto.getMemberGrade())//회원등급
-						.accessToken(accessToken)//JWT 토큰
-						.refreshToken(refreshToken)//JWT 토큰
-					.build());//200
-		}
-		else {//실패
-			return ResponseEntity.status(401).build();//401
-		}
-		
-	}
-	
-	//refresh token으로 로그인하는 매핑
-		//- header에 있는 Authorzation이라는 항목을 읽어 해석한 뒤 결과를 반환
-		//- 토큰이 만료되었다면(잘목된 토큰/시간지남/....)401반환
-		@PostMapping("/refresh")
-		public ResponseEntity<MemberLoginVO> refresh(
-				@RequestHeader("Authorization") String refreshToken){
-			try {
-				MemberLoginVO loginVO = jwtService.parse(refreshToken);
-				//loginsVO에있는 정보가 실제 DB와 일치하는지 추가적으로 조회
-				MemberDto memberDto = memberDao.selectOne(loginVO.getMemberNo());
-				if(memberDto == null) {//존재하지 않는 아이디
-					throw new Exception("존재하지 않는 아이디");
-			}
-			if(!loginVO.getMemberGrade().equals(memberDto.getMemberGrade())) {
-				throw new Exception("정보 불일치");
-			}
-			//위에서 필터링되지 않았다면 refresh token이 유효하다고 볼 수 있다
-			//-> 사용자에게 새롭게 access token을 발급한다
-			//-> 보안을 위해서 refresh token도 재발급한다
-			String accessToken = jwtService.createAccessToken(memberDto);
-			String newRefreshToken = jwtService.createRefreshToken(memberDto);
-			return ResponseEntity.ok().body(MemberLoginVO.builder()
-						.memberNo(memberDto.getMemberNo())
-						.memberGrade(memberDto.getMemberGrade())
-						.accessToken(accessToken)
-						.refreshToken(newRefreshToken)
-					.build());
-			}
-			catch(Exception e) {//잘못된 토큰
-				return ResponseEntity.status(401).build();
-			}
-			
-		}
 	//아이디 중복체크
 	@GetMapping("/doubleCheckId/{memberId}")
 	public boolean selectDoubleCheckId(@PathVariable String memberId) {
@@ -160,5 +101,35 @@ public class MemberRestController {
 		return emailService.sendCert(memberEmail);
 	}
 	
-	///브랜치 테스트
+	//로그인
+	@PostMapping("/login")
+	public ResponseEntity<MemberLoginVO> login(@RequestBody MemberDto memberDto) {
+		//회원번호로 정보조회
+		MemberDto findDto = memberDao.selectOne(memberDto.getMemberNo());
+		if(findDto == null) {//아이디없음(404)
+			return ResponseEntity.notFound().build();//404
+		}
+		
+		//비밀번호 비교
+		boolean isValid = findDto.getMemberPw().equals(memberDto.getMemberPw());
+		
+		if(isValid) {//성공- MemberLoginVO(200)
+			return ResponseEntity.ok().body(MemberLoginVO.builder()
+						.memberNo(findDto.getMemberNo())
+						.memberGrade(findDto.getMemberGrade())
+					.build());//200
+		} else {//실패- 파라미터오류(400)/ 미인증(401)
+			return ResponseEntity.status(401).build();//401
+		}
+ 		
+	}
+	
 }
+
+
+
+
+
+
+
+
--- kh13fb/src/main/java/com/kh/kh13fb/vo/MemberLoginVO.java
+++ kh13fb/src/main/java/com/kh/kh13fb/vo/MemberLoginVO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//로그인에 성공한 회원에게 전달할 정보
 @Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class MemberLoginVO {
 
