bfd000bd8431209f17627c93b197ef1eb499fb44
yellll1n <yr1230_1103@hanmail.net>
2024-05-19 18:04:58 +0900

    ConcertRequest 이미지 가지고 오기

--- kh13fb/src/main/java/com/kh/kh13fb/service/AttachService.java
+++ kh13fb/src/main/java/com/kh/kh13fb/service/AttachService.java
@@ -2,9 +2,17 @@ package com.kh.kh13fb.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.kh13fb.configuration.FilePathProperties;
@@ -47,6 +55,50 @@ public class AttachService {
 		File target = new File(dir, String.valueOf(attachNo));
 		target.delete();
 		attachDao.delete(attachNo);//파일 정보 지우기
+	}
+
+	public ResponseEntity<ByteArrayResource> download(
+			@RequestParam int attachNo) throws IOException{
+	
+		//1. attachNo로 파일 정보(AttachDto)를 불러온다
+		AttachDto attachDto = attachDao.selectOne(attachNo);
+		
+		//2. attachDto가 없으면 404처리
+		if(attachDto == null) {
+		//	return ResponseEntity.status(404).build();
+			return ResponseEntity.notFound().build();
+		}
+		//3. 실제 파일을 불러온다 (apache common io, apache commons fileupload)
+		File dir = new File(filePathProperties.getPath());
+		File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
+		
+		byte[] data = FileUtils.readFileToByteArray(target);//파일을 읽어라
+		ByteArrayResource resource = new ByteArrayResource(data);//포장
+		
+		//4. attachDto에있는 정보와 실제 파일(3번) 정보를 조합하여 사용자에게  
+		//- 추가적인 정보를 제공해서 파일 다운로드 처리가 일어나도록 구현
+		//- 추가적인 정보는 header에 설정
+		
+		//return ResponseEntity.status(200).body(내용);
+		//return ResponseEntity.ok()
+		//		.header("Content-Encoding", "UTF-8")
+		//		.header("Content-Type", attachDto.getAttachType())
+		//		.header("Content-Type", "application/octet-stream")//무조건 다운로 	
+		//		.header("Content-Length", String.valueOf(attachDto.getAttachSize()))
+		//		.header("Content-Disposition", "attachment; filename="+attachDto.getAttachName())
+		//		.body(resource);
+		
+		return ResponseEntity.ok()
+			.header(HttpHeaders.CONTENT_ENCODING,StandardCharsets.UTF_8.name())
+		//	.header(HttpHeaders.CONTENT_TYPE, attachDto.getAttachType())	
+			.contentType(MediaType.APPLICATION_OCTET_STREAM)
+			.contentLength(attachDto.getAttachSize())
+			.header(HttpHeaders.CONTENT_DISPOSITION,
+					ContentDisposition.attachment()
+					.filename(attachDto.getAttachName(), StandardCharsets.UTF_8)
+					.build().toString()
+			)
+			.body(resource);
 	}
 
 }
